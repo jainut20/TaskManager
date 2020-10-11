a379ed61b98bc3f550abb193cc70f8f44998cc43
jainut20 <58257957+jainut20@users.noreply.github.com>
2020-10-11 22:05:48 +0530

    Update mongoose.js

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://Utkarsh:Utkarsh@cluster0-jawby.mongodb.net/<dbname>?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(process.env.MONGODB_URI || "", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Mongodb connection established')
 }).catch((e) => {
     console.log(e)
